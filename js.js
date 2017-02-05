f0c780f54490a5427ec25be529cc51b9d88870c8
Vatsala Shrivastava <vatsalashrivastava12@gmail.com>
2017-02-05 10:21:56 +0530

    Finally done!!
    More codes

---
+++ js.js
@@ -0,0 +1,13 @@
+<html>
+<head></head>
+<body>
+<script language="javascript">
+var a,b;
+a=prompt("Enter frst num");
+a=parseInt(a);
+b=prompt("Enter scnd num");
+b=parseInt(b);
+document.write("Sum="+(a+b));
+</script>
+</body>
+</html>
